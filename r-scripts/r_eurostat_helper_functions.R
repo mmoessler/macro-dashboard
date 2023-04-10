264708139e664a37709977b20164e9828896eb1c
mmoessler <markus.moessler@gmail.com>
2023-04-10 13:43:45 +0200

    removed specific code from function to download from eurostat

--- r-scripts/r_eurostat_helper_functions.R
+++ r-scripts/r_eurostat_helper_functions.R
@@ -1,6 +1,15 @@
 
 get_eurostat_xx <- function(url) {
 
+  # # input
+  # fre <- "month"
+  # url <- "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/sts_inpr_m?format=JSON&geo=DE&geo=FR&geo=EA19&freq=M&unit=I15&s_adj=SCA&indic_bt=PROD&nace_r2=B-D"
+  # 
+  # fre <- "quarter"
+  # url <- "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/namq_10_gdp?format=JSON&geo=DE&geo=FR&geo=EA19&freq=Q&unit=CLV15_MEUR&na_item=B1G&s_adj=SCA"
+
+      
+  
   type <- c("code")
   stringsAsFactors <- FALSE
 
@@ -30,13 +39,7 @@ get_eurostat_xx <- function(url) {
   
   dat$values[inds] <- vals
   data <- tibble::as_tibble(dat)
-  data
-  
-  data$time <- as.Date(as.yearmon(data$time))
-  
-  ser.dat <- subset(data, time %in% seq(as.Date("2015-01-01"), Sys.Date(), by = "month"))
-  head(ser.dat)
-  
-  return(ser.dat)
+
+  return(data)
   
 }
